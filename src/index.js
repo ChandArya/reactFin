b07db2be430ba2906f4f9a9efd90cd8ad321c231
ChandArya <chandangupta1010@gmail.com>
2020-04-29 20:27:12 +0530

    Login page design
    login screen

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-/*!
 
-=========================================================
-* Material Dashboard React - v1.8.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
@@ -23,6 +7,7 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 // core components
 import Admin from "layouts/Admin.js";
 import RTL from "layouts/RTL.js";
+import Login from "layouts/Login.js";
 
 import "assets/css/material-dashboard-react.css?v=1.8.0";
 
@@ -33,7 +18,9 @@ ReactDOM.render(
     <Switch>
       <Route path="/admin" component={Admin} />
       {/* <Route path="/rtl" component={RTL} /> */}
-      <Redirect from="/" to="/admin/dashboard" />
+      <Redirect from="/dashbord" to="/admin/dashboard" />
+
+      <Route from="/"  component={Login}/>
     </Switch>
   </Router>,
   document.getElementById("root")
